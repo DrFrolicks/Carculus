0438eeca6796adc01aa41e5f775250d6e430d785
Frolicks <dr.frolicks@yahoo.com>
2018-05-20 14:23:44 -0700

    player spawning started

---
+++ Carculus/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+    public static PlayerManager instance;
+    public GameObject PlayerPrefab;
+    public Transform[] spawnLocations; 
+
+    public int numOfPlayers; 
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this; 
+    }
+
+    public void spawnPlayer()
+    {
+        if (numOfPlayers >= spawnLocations.Length)
+            return;
+
+        Player p = Instantiate(PlayerPrefab, spawnLocations[numOfPlayers].position, Quaternion.identity).GetComponent<Player>();
+        p.index = numOfPlayers;  
+        numOfPlayers++;  
+    }
+}
